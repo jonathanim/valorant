b862ecb07673c2c2858db66c0ff615f9e3a5c688
jonathanim <imjonathan.work@gmail.com>
2022-05-22 21:26:52 -0700

    center  youtube player  in character details

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,33 +1,16 @@
-import React from "react";
+import React from 'react'
 
 const CharacterCard = ({ character }) => {
-  return (
-    <div>
-      <div className="card transition-all border-2 p-5 m-3 hover:scale-105">
-        <div
-          className="image-ctn rounded"
-          style={{
-            backgroundImage: `url(${character.background})`,
-            backgroundPosition: "center center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundColor: "#fa4454",
-          }}
-        >
-          <img src={character.fullPortrait} alt={character.displayName} />
-        </div>
+    return (
+        <div className='card'>
+            <img src={character.fullPortrait} alt={character.displayName} />
+            <h2 className='text-center p-2 text-xl'>{character.displayName}</h2>
+            <div className="role">
+                <p className='text-center p-2 text-xl3'>{character.role.displayName}</p>
+            </div>
 
-        <h2 className="text-center p-2 text-2xl font-bold tracking-wide">
-          {character.displayName}
-        </h2>
-        <div className={`role`}>
-          <p className="text-center p-2 text-xl">
-            {character.role.displayName}
-          </p>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default CharacterCard;
+export default CharacterCard
--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -5,109 +5,109 @@ import YouTube from "react-youtube";
 import Loading from "./Loading";
 
 const videos = {
-  Breach: "Rux0HjzKQbw",
-  Raze: "2-uAL__pp_U",
-  Chamber: "LVoF64LOgL0",
-  "KAY/O": "r0n4AYekCaA",
-  Skye: "C3QTyMXi-WE",
-  Cypher: "SgbqFtRRgLA",
-  Sova: "OZ76UP-c8Ao",
-  Killjoy: "ua-iIRQDY8g",
-  Viper: "9dOSy0EhLfQ",
-  Phoenix: "ttJMFW2wUQM",
-  Astra: "-ylVnuPWlJM",
-  Brimstone: "7yHnJ_oNxTI",
-  Yoru: "GdOEQv-zQVw",
-  Sage: "1aRwM_QsqQI",
-  Reyna: "IzsQNYrq9AM",
-  Omen: "_jJdWy6bDj4",
-  Jett: "-cPLXswVsvc",
-  Fade: "UMzKD73cs3c",
-  Neon: "dtx8CgjRmqE",
+    Breach: "Rux0HjzKQbw",
+    Raze: "2-uAL__pp_U",
+    Chamber: "LVoF64LOgL0",
+    "KAY/O": "r0n4AYekCaA",
+    Skye: "C3QTyMXi-WE",
+    Cypher: "SgbqFtRRgLA",
+    Sova: "OZ76UP-c8Ao",
+    Killjoy: "ua-iIRQDY8g",
+    Viper: "9dOSy0EhLfQ",
+    Phoenix: "ttJMFW2wUQM",
+    Astra: "-ylVnuPWlJM",
+    Brimstone: "7yHnJ_oNxTI",
+    Yoru: "GdOEQv-zQVw",
+    Sage: "1aRwM_QsqQI",
+    Reyna: "IzsQNYrq9AM",
+    Omen: "_jJdWy6bDj4",
+    Jett: "-cPLXswVsvc",
+    Fade: "UMzKD73cs3c",
+    Neon: "dtx8CgjRmqE",
 };
 
 const CharacterDetails = () => {
-  const [character, setCharacter] = useState({});
-  const [loading, setLoading] = useState(true);
-  const { id } = useParams();
-  const [video, setVideo] = useState("");
+    const [character, setCharacter] = useState({});
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [video, setVideo] = useState("");
 
-  const opts = {
-    height: "390",
-    width: "640",
-    playerVars: {
-      autoplay: 0,
-    },
-  };
+    const opts = {
+        height: "390",
+        width: "640",
+        playerVars: {
+            autoplay: 0,
+        },
+    };
 
-  const onReadyP = (event) => {
-    event.target.pauseVideo();
-  };
+    const onReadyP = (event) => {
+        event.target.pauseVideo();
+    };
 
-  const fetchCharacter = async () => {
-    const characterInfo = await axios.get(
-      `https://valorant-api.com/v1/agents/${id}`
-    );
+    const fetchCharacter = async () => {
+        const characterInfo = await axios.get(
+            `https://valorant-api.com/v1/agents/${id}`
+        );
 
-    setCharacter(characterInfo.data.data);
-    setLoading(false);
-  };
+        setCharacter(characterInfo.data.data);
+        setLoading(false);
+    };
 
-  useEffect(() => {
-    fetchCharacter();
-    setVideo(videos[character.displayName]);
-  }, [character]);
+    useEffect(() => {
+        fetchCharacter();
+        setVideo(videos[character.displayName]);
+    }, [character]);
 
-  if (loading) {
-    return (
-      <h1>
-        <Loading />
-      </h1>
-    );
-  } else {
-    return (
-      <div className="container mx-auto text-center justify-center items-center">
-        <h1 className="text-4xl m-5">{character.displayName}</h1>
-        <div className="container mx-auto grid justify-center items-center">
-          <div className="justify-center items-center">
-            <YouTube
-              videoId={video}
-              opts={opts}
-              onReady={(e) => () => onReadyP}
-              className="border-4 p-3 bg-black block"
-            />
-          </div>
-          <h2 className="text-3xl my-4">Skills</h2>
-          <div className="block sm:flex">
-            {character.abilities.map((skill, i) => {
-              return (
-                <div
-                  key={i}
-                  className="mb-3 p-2 bg-orange-300 text-black items-center"
-                >
-                  <img
-                    src={skill.displayIcon}
-                    alt={skill.displayName}
-                    style={{
-                      width: "100px",
-                      height: "100px",
-                      margin: "auto",
-                      objectFit: "scale-down",
-                    }}
-                  />
-                  <h3 className="text-3xl">{skill.displayName}</h3>
+    if (loading) {
+        return (
+            <h1>
+                <Loading />
+            </h1>
+        );
+    } else {
+        return (
+            <div className="container mx-auto text-center justify-center items-center">
+                <h1 className="text-4xl m-5">{character.displayName}</h1>
+                <div className="container mx-auto grid justify-center items-center">
+                    <div className="flex justify-center items-center">
+                        <YouTube
+                            videoId={video}
+                            opts={opts}
+                            onReady={(e) => () => onReadyP}
+                            className="border-4 p-3 bg-black block"
+                        />
+                    </div>
+                    <h2 className="text-3xl my-4">Skills</h2>
+                    <div className="block sm:flex">
+                        {character.abilities.map((skill, i) => {
+                            return (
+                                <div
+                                    key={i}
+                                    className="mb-3 p-2 bg-orange-300 text-black items-center"
+                                >
+                                    <img
+                                        src={skill.displayIcon}
+                                        alt={skill.displayName}
+                                        style={{
+                                            width: "100px",
+                                            height: "100px",
+                                            margin: "0 auto",
+                                            objectFit: "scale-down",
+                                        }}
+                                    />
+                                    <h3 className="text-3xl">{skill.displayName}</h3>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center mt-10 p-5">
-          <h2 className="text-3xl w-64 text-center mb-2">Summary</h2>
-          <p className="w-2/4 text-center">{character.description}</p>
-        </div>
-      </div>
-    );
-  }
+                <div className="flex flex-col justify-center items-center mt-10 p-5">
+                    <h2 className="text-3xl w-64 text-center mb-2">Summary</h2>
+                    <p className="w-2/4 text-center">{character.description}</p>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default CharacterDetails;
